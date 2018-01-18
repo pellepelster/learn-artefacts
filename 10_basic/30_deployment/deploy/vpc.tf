de3ff982ecb57453f8d56b0b86314c86f758493a
Christian Pelster <pelle@pelle.io>
2018-01-18 18:54:18 +0100

    new artefacts

---
+++ 10_basic/30_deployment/deploy/vpc.tf
@@ -0,0 +1,29 @@
+resource "aws_vpc" "todo_vpc" {
+  cidr_block           = "10.0.0.0/16" //snippet:deploy_aws_vpc
+  enable_dns_hostnames = true
+} //eos:deploy_aws_vpc
+
+resource "aws_subnet" "public_subnet" {
+  vpc_id                  = "${aws_vpc.todo_vpc.id}" //snippet:deploy_aws_public_subnet
+  cidr_block              = "10.0.0.1/24"
+  availability_zone       = "eu-central-1a"
+  map_public_ip_on_launch = true
+} //eos:deploy_aws_public_subnet
+
+resource "aws_internet_gateway" "todo_internet_gateway" {
+  vpc_id = "${aws_vpc.todo_vpc.id}" //snippet:deploy_aws_routing
+}
+
+resource "aws_route_table" "todo_main_route_table" {
+  vpc_id = "${aws_vpc.todo_vpc.id}"
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.todo_internet_gateway.id}"
+  }
+}
+
+resource "aws_main_route_table_association" "todo_main_route_table_association" {
+  vpc_id         = "${aws_vpc.todo_vpc.id}"
+  route_table_id = "${aws_route_table.todo_main_route_table.id}"
+} //eos:deploy_aws_routing
