3896e869417e42e92b5c2ec5ccb8ca9526cdf119
Christian Pelster <pelle@pelle.io>
2018-01-23 22:50:57 +0100

    new artefacts

---
+++ 10_basic/10_the_assignment/todo-frontend/src/components/TodoList.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <todo-item v-bind:message="item.message" v-for="item in items"></todo-item>
+  </div>
+</template>
+
+<script>
+import TodoItem from '@/components/TodoItem'
+
+export default {
+  name: 'TodoList',
+  components: {
+    'todo-item': TodoItem
+  },
+  data () {
+    return { items: [
+      { message: 'Foo' },
+      { message: 'Bar' }
+    ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
---
+++ 10_basic/30_deployment/todo-frontend/src/components/TodoItem.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>{{ message }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TodoItem',
+  props: ['message']
+}
+</script>
+
+<style scoped>
+</style>
