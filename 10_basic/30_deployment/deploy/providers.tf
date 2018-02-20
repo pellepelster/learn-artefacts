b430497770b99e26b957c3f8c378c7ab83ab707d
Christian Pelster <pelle@pelle.io>
2018-02-20 08:52:22 +0100

    remove snippet markers

--- 10_basic/30_deployment/deploy/providers.tf
+++ 10_basic/30_deployment/deploy/providers.tf
@@ -1,5 +1,3 @@
-# snippet:deploy_aws_provider
 provider "aws" {
   region = "eu-central-1"
 }
-# /snippet:deploy_aws_provider
--- 10_basic/30_deployment/deploy/vpc.tf
+++ 10_basic/30_deployment/deploy/vpc.tf
@@ -1,19 +1,14 @@
-# snippet:deploy_aws_vpc
 resource "aws_vpc" "todo_vpc" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_hostnames = true
 }
-# /snippet:deploy_aws_vpc
 
-# snippet:deploy_aws_public_subnet
 resource "aws_subnet" "public_subnet" {
   vpc_id                  = "${aws_vpc.todo_vpc.id}"
   cidr_block              = "10.0.0.1/24"
   map_public_ip_on_launch = true
 }
-# /snippet:deploy_aws_public_subnet
 
-# snippet:deploy_aws_routing
 resource "aws_internet_gateway" "todo_internet_gateway" {
   vpc_id = "${aws_vpc.todo_vpc.id}"
 }
@@ -31,4 +26,3 @@ resource "aws_main_route_table_association" "todo_main_route_table_association" 
   vpc_id         = "${aws_vpc.todo_vpc.id}"
   route_table_id = "${aws_route_table.todo_main_route_table.id}"
 }
-# /snippet:deploy_aws_routing
